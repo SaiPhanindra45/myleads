0385073a7f7e7d902cfdad25ec6f440c14421657
SaiPhanindra45 <phanindrapersonal02@gmail.com>
2025-02-25 17:53:33 +0530

    savetab button and some other changes

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
-let leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-})
+const dltbtn = document.getElementById("dlt-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if(leadsfromlocalStorage){
     myLeads = leadsfromlocalStorage
-    renderLeads()
+    render(myLeads)
 }
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
 }
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+dltbtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
